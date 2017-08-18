3914d9c05dd268f96c56fad602f2c00795fa663d
Megan Paik <paik.megan@gmail.com>
2017-08-17 20:14:02 -0700

    YAY STORAGE FONTS

--- fontjquery.js
+++ fontjquery.js
@@ -1,8 +1,13 @@
+var currFont = window.getComputedStyle(document.body, null).getPropertyValue('font-family');
+
 $(function() {
     $('#font').fontselect().change(function() {
         var font = $(this).val().replace(/\+/g,' ');
         font = font.split(':');
         $('body').css('font-family', font[0]);
         $('button').css('font-family', font[0]);
+        chrome.storage.sync.set({currFont: font[0]}, function() {
+          console.log("font saved" + font[0]);
+        });
     });
 });
--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 document.body.onload = function() {
   chrome.storage.sync.get(null, function(items) {
     console.log(items);
-    for (var key in items) {
-      addExistingTask(items[key]);
+      for (var key in items) {
+      if(key === 'currFont') {
+        $('link:last').after('<link href="https://fonts.googleapis.com/css?family=' + items[key] + '" rel="stylesheet" type="text/css">');
+        $('body').css('font-family', items[key]);
+        $('button').css('font-family', items[key]);
+        console.log("font changed on load");
+        continue;
+      }
+      if (key === 'currColor')  {
+        document.body.style.backgroundColor = items[key];
+        console.log("changed color on load");
+        continue;
+      }
+      else {
+        addExistingTask(items[key]);        
+      }
     }
   });
 }
@@ -10,6 +24,8 @@ var taskInput = document.getElementById("new-task");
 var addButton = document.getElementsByTagName("button")[0];
 var allTasks = document.getElementById("tasks");
 var taskEdit = document.getElementById("");
+var currColor = document.body.style.backgroundColor;
+
 
 //add new task to list
 var addNewTask = function(userInput) {
@@ -103,7 +119,6 @@ var deleteTask = function() {
   var ul = task.parentNode;
   var editInput = task.querySelector("label");
   var keyStr = editInput.innerText;
-  console.log(keyStr);
   chrome.storage.sync.remove(keyStr, function() {
     console.log("deleted from storage");
   });
@@ -148,29 +163,51 @@ for(var i = 0; i <  allTasks.children.length; i++) {
 
 
 // The buttons to change background color
-
+var storeColor = function(currColor) {
+  chrome.storage.sync.set({currColor: currColor}, function() {
+    console.log('color successfully stored' + currColor);
+  })
+}
 var color1 = document.getElementById("color1");
-color1.addEventListener("click", function() { document.body.style.backgroundColor = "#ff857a";
+color1.addEventListener("click", function() { 
+  document.body.style.backgroundColor = "#ff857a";
+  currColor = "#ff857a";
+  storeColor(currColor);
 });
 
 var color2 = document.getElementById("color2");
-color2.addEventListener("click", function() { document.body.style.backgroundColor = "#ffe47a";
+color2.addEventListener("click", function() { 
+  document.body.style.backgroundColor = "#ffe47a"; 
+  currColor = "#ffe47a";
+  storeColor(currColor);
 });
 
 var color3 = document.getElementById("color3");
-color3.addEventListener("click", function() { document.body.style.backgroundColor = "#a0f98b";
+color3.addEventListener("click", function() { 
+  document.body.style.backgroundColor = "#a0f98b";
+  currColor = "#a0f98b";
+  storeColor(currColor);
 });
 
 var color4 = document.getElementById("color4");
-color4.addEventListener("click", function() { document.body.style.backgroundColor = "#8be5f9";
+color4.addEventListener("click", function() { 
+  document.body.style.backgroundColor = "#8be5f9";
+  currColor = "#8be5f9";
+  storeColor(currColor);
 });
 
 var color5 = document.getElementById("color5");
-color5.addEventListener("click", function() { document.body.style.backgroundColor = "#d8a4f4";
+color5.addEventListener("click", function() { 
+  document.body.style.backgroundColor = "#d8a4f4";
+  currColor = "#d8a4f4";
+  storeColor(currColor);
 });
 
 var color6 = document.getElementById("color6");
-color6.addEventListener("click", function() { document.body.style.backgroundColor = "#FFC0CB";
+color6.addEventListener("click", function() { 
+  document.body.style.backgroundColor = "#FFC0CB";
+  currColor = "#FFC0CB";
+  storeColor(currColor);
 });
 
 
@@ -190,3 +227,4 @@ toggler.onclick = function() {
         div.style.display = 'block';
     }
 };
+
